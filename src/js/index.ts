ef22900630805625da1243490b6967fffc607e9a
Anina Mahat <anin0543@edu.easj.dk>
2018-10-29 14:45:00 -0700

    changes
    changes

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,33 @@
-interface Person {
-    firstName: string;
-    lastName: string;
-}
+import axios, {
+    AxiosResponse,
+    AxiosError}
+    from "../../node_modules/axios/index";
+    
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
-
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+    interface IBid{
+        Id:number;
+        Item:string;
+        Price:number;
+        Name:string;
+    }
+    let getAllButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("AllBidsButton")
+    getAllButton.addEventListener("click", doGetAll);
+    let outputElement :AddEventListenerOptions("click" , doGetAll);
+    
+    function doGetAll(): void {
+        let uri: string = "https://restcoinservice20181029121431.azurewebsites.net/api/Bid";
+        axios.get<IBid>(uri)
+            .then(function (response: AxiosResponse): void {
+                console.log(response.data);
+                let result: string = "<ol>";
+                response.data.array.forEach((bids: IBid) => {
+                    result += "<li>" + bids.Id + bids.Item + bids.Price + ' ' + bids.Name +"</li>";
+                });
+                result += "</ol>"
+                console.log(result);
+            })
+    
+            .catch(function (error: AxiosError): void {
+                console.log(error);
+            });
+    }
